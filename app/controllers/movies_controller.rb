c150925427500366a808ba20338ab1aed026857d
Todd Kennedy <todd@selfassembled.org>
2020-05-21 10:56:18 -0700

    updates

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
   def show
-    @movie = Movie.find(params[:id])
+    movie = Movie.find(params[:id])
+    render :show, locals: { movie: movie }
+  end
+
+  def index
+    movies = Movie.all
+    render :index, locals: { movies: movies }
   end
 end
--- test/system/movies_system_test.rb
+++ test/system/movies_system_test.rb
@@ -18,4 +18,20 @@ class MoviesSystemTest < ApplicationSystemTestCase
     assert_text 'Titanic'
     assert_text 'James Cameron'
   end
+
+  test 'visiting the index page' do
+    # create two movies
+    # visit index page
+    # assert we can see them
+    d1 = Director.create!(name: 'Boon Jong-Ho')
+    Movie.create!(title: 'Parasite', director_id: d1.id)
+    d2 = Director.create!(name: 'James Cameron')
+    Movie.create!(title: 'Titanic', director_id: d2.id)
+
+    visit('/movies/')
+    assert_text 'Parasite'
+    assert_text 'Boon Jong-Ho'
+    assert_text 'Titanic'
+    assert_text 'James Cameron'
+  end
 end
